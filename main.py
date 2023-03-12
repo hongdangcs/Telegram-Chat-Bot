36cc0606f53a6e3f7fed2c711f01882d78ccc5f8
hongdangcseiu <hongdang2k1@gmail.com>
2023-03-13 03:01:59 +0700

    page screenshot

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import Constants as key
+import telebot
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bot = telebot.TeleBot(key.API_KEY)
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.reply_to(message, "/help for help")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, "Sorry, I can't understand you! /help")
+	print(message.chat.id)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+bot.infinity_polling()
---
+++ puppeteer-capture.js
@@ -0,0 +1,72 @@
+const puppeteer = require("puppeteer");
+const fs = require('fs');
+const args = process.argv.slice(2);
+puppeteer
+    .launch({
+        defaultViewport: {
+            width: 1920,
+            height: 1080,
+        },
+    })
+    .then(async (browser) => {
+        const page = await browser.newPage();
+        /*
+        await page.goto('file://C:\\Users\\PHAMHONGDANG\\PycharmProjects\\TelegramChatBot\\Stock\\index.html');
+        await delay(500);
+        await page.screenshot({path: "photo/entire-page.png"});
+
+         */
+
+        await page.goto("https://fireant.vn/home/content/symbols/VNINDEX");
+        await delay(200);
+
+
+        const selector = await page.$$(".bp3-dialog-close-button");
+        await selector[1].click();
+        await delay(100);
+
+        const tab = await page.$$(".sc-eqKtKx .bp3-tab");
+        await tab[5].click();
+        console.log(tab.length);
+        await delay(2000);
+
+        page.waitForXPath('/html/body/div[2]/div[3]/div/div/div[1]/div/div/div/div/div/div[7]').then(() => console.log("have a xpath"));
+
+
+
+        /*
+                const selector2 = ".bp3-portal";
+                await page.evaluate(`document.querySelector("${selector2}").remove()`);
+        */
+
+        await page.screenshot({path: "photo/fireant-page.png"});
+        /*
+        await delay(250);
+        await page.mouse.click(228, 21)
+        await delay(500);
+
+        await page.mouse.click(310, 387)
+        await delay(500);
+
+        const element = await page.waitForSelector('div > .chart-container');
+        await element.screenshot({path: "photo/" + args[1] + ".png"});
+
+        const inner_currency = await page.$eval('.currency-qqt8UV2f', element => element.innerHTML);
+
+        const inner_price_html = await page.$eval('.buttonText-_W8EGxGy', element => element.innerHTML);
+
+        let inner_price = inner_price_html.replace('<span class=\"highlight-vZ7uMuW4 growing-vZ7uMuW4\">', '');
+        inner_price = inner_price.replace('</span>', '');
+
+        let price = inner_price + inner_currency;
+
+        fs.writeFileSync(args[1] + 'price.txt', price);
+        console.log(price);
+
+         */
+        await browser.close();
+    });
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
